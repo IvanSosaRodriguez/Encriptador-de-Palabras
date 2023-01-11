77af701d410a29dbee7eef2ee00721b357c49f18
Ivan <ivandejesussosarodriguez@gmail.com>
2023-01-11 13:42:13 -0400

    display output

--- script.js
+++ script.js
@@ -3,12 +3,17 @@ function encriptar (){
     var textoCifrado = texto.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat");
     document.querySelector("#output-text").value = textoCifrado;
     document.querySelector("#input-text").value;
+    let i = true
+    if(i=true){
+        document.getElementById("esconder").style.display = "none";
+        document.getElementById("output-text").style.display = "block";
+        i=false
     }
+}
 
 var boton1 = document.querySelector("#button-a"); 
 
-boton1.onclick = encriptar,esconder;
-boton1.onclick = 
+boton1.onclick = encriptar;
 
 function desencriptar (){ var texto = document.querySelector("#input-text").value; 
 var textoCifrado = texto.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ai/gi, "a").replace(/ober/gi, "o").replace(/ufat/gi, "u"); 
