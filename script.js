1df25394668b993e86b57a0bf6905dcfca5e075a
Ivan <ivandejesussosarodriguez@gmail.com>
2023-01-11 13:13:11 -0400

    agregado el js con el encriptador

---
+++ script.js
@@ -0,0 +1,22 @@
+function encriptar (){
+    var texto = document.querySelector("#input-text").value;
+    var textoCifrado = texto.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat");
+    document.querySelector("#output-text").value = textoCifrado;
+    document.querySelector("#input-text").value;
+    }
+
+var boton1 = document.querySelector("#button-a"); 
+
+boton1.onclick = encriptar,esconder;
+boton1.onclick = 
+
+function desencriptar (){ var texto = document.querySelector("#input-text").value; 
+var textoCifrado = texto.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ai/gi, "a").replace(/ober/gi, "o").replace(/ufat/gi, "u"); 
+document.querySelector("#output-text").value = textoCifrado; 
+document.querySelector("#input-text").value;
+
+}
+
+var boton2 = document.querySelector("#button-b"); 
+
+boton2.onclick = desencriptar;
